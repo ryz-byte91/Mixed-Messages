89fdbb4771ca1cd0287f3232473a8f7d74032a0a
Rylea Z <ryzart17@gmail.com>
2024-01-15 20:11:44 -0500

    -m "modified to run on javascript only"

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const spiritAnimals = ['wolf', 'lion', 'tiger', 'owl', 'gazell', 'otter', 'hummi
 const chooseAnimal = (arr) => {
     let randNum = Math.floor(Math.random() * 9); 
     let userAnimal = arr[randNum];
-    return userAnimal;
-} 
+    console.log(greeting);
+    console.log(userAnimal);
 
-console.log(greeting);
+ }; 
 
-let button = document.getElementById('button');
-button.onclick = chooseAnimal(spiritAnimals);
+
+
+
+
